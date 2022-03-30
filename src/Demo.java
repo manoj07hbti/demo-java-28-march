06d28f1270f31213d3472c85e891e3a05d68ea44
60manojkumar@gmail.com <Motivated@2019>
2022-03-30 19:16:31 +0530

    Initial commit

---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my first java Program");
+
+        System.out.println("Hello");
+
+    }
+
+}
+
+
+
