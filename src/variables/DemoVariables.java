b4643be5ad97bf06688297c55343998e536ee69c
60manojkumar@gmail.com <Motivated@2019>
2022-03-31 19:23:01 +0530

    test file

---
+++ src/variables/DemoVariables.java
@@ -0,0 +1,29 @@
+package variables;
+
+public class DemoVariables {
+
+    public static void main(String[] args) {
+
+        // SYNTAX:  DataType var_name= value;
+
+       System.out.println("Printing Variable values ");
+
+       String city="AGRA";
+       System.out.println("Printing city: " +city);
+
+       String name="Raj";
+       System.out.println("Printing name:" +name);
+
+       int age= 23;
+       System.out.println("Printing age: " +age);
+       int marks=75;
+       System.out.println("Printing marks: " +marks);
+
+
+       double price=25.3;
+       System.out.println("Printing price: " +price);
+       double salary=56000.13;
+       System.out.println("Printing salary: "+salary);
+
+    }
+}
