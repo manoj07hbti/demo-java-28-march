dbaf6c4a726219c576de2bc88fae443e1679ee9e
60manojkumar@gmail.com <Motivated@2019>
2022-04-01 19:33:49 +0530

    java class

---
+++ src/variables/MyClass.java
@@ -0,0 +1,11 @@
+package variables;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        String name= "Java";
+        System.out.println(" My Message "+name);
+
+    }
+}
