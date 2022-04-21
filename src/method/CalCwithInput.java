ee83b494461cb198094cd18aa2c1259d61dc7956
60manojkumar@gmail.com <Motivated@2019>
2022-04-21 19:52:39 +0530

    scanner

--- src/method/CalCwithInput.java
+++ src/method/CalCwithInput.java
@@ -1,5 +1,7 @@
 package method;
 
+import java.util.Scanner;
+
 public class CalCwithInput {
 
     public int add(int a, int b){
@@ -12,7 +14,13 @@ public class CalCwithInput {
 
         CalCwithInput obj= new CalCwithInput();
 
-       int result= obj.add(3,9);
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Please enter value of a:");
+        int a= scanner.nextInt();
+        System.out.println("Please enter value of b:");
+        int b= scanner.nextInt();
+       int result= obj.add(a,b);
        System.out.println("Addition is : "+result);
         result= obj.add(23,12);
         System.out.println("Addition is : "+result);
