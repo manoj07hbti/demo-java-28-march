fdeb5a90387bee8fa30241669aeb63ffb6d042c2
60manojkumar@gmail.com <Motivated@2019>
2022-04-23 19:29:33 +0530

    infinite loop Calc

--- src/method/CalCwithInput.java
+++ src/method/CalCwithInput.java
@@ -16,14 +16,30 @@ public class CalCwithInput {
 
 
         Scanner scanner= new Scanner(System.in);
-        System.out.println("Please enter value of a:");
-        int a= scanner.nextInt();
-        System.out.println("Please enter value of b:");
-        int b= scanner.nextInt();
-       int result= obj.add(a,b);
-       System.out.println("Addition is : "+result);
-        result= obj.add(23,12);
-        System.out.println("Addition is : "+result);
+        int flag=0;
+        int result=0;
+       while (flag==0) {
 
+           System.out.println("Please enter 1: addition, 2: for subtraction , 3: multiply and 4 for divide..");
+
+           int choie=scanner.nextInt();
+
+           System.out.println("Please enter value of a:");
+           int a = scanner.nextInt();
+           System.out.println("Please enter value of b:");
+           int b = scanner.nextInt();
+
+           if(choie==1){
+            result = obj.add(a, b);
+            System.out.println("Addition of a and b is :"+result);
+           }
+           else if(choie==2){
+               // subtraction call
+           }
+
+           System.out.println("Press 1 to exit from App or press 0 to continue...");
+           Scanner scanner1= new Scanner(System.in);
+           flag=scanner1.nextInt();
+       }
     }
 }
