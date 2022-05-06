344abab29a95cd2545edfeeb4a2315c3456782ea
60manojkumar@gmail.com <Motivated@2019>
2022-05-06 20:09:58 +0530

    Method overriding

--- src/polymorphism/DemoA.java
+++ src/polymorphism/DemoA.java
@@ -1,5 +1,7 @@
 package polymorphism;
 
+// Runtime Polymorphism: Method overriding -> we will override (changing body of method )same method in child class
+
 public class DemoA {
 
     public void m1(){
